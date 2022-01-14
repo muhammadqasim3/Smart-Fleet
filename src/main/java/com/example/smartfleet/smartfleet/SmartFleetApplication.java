6bcc48c581931949df280da0376cca4f3138077d
Muhammad Qasim <muhammadqasimaslam3@gmail.com>
2022-01-14 21:39:12 +0500

    test

---
+++ src/main/java/com/example/smartfleet/smartfleet/SmartFleetApplication.java
@@ -0,0 +1,14 @@
+package com.example.smartfleet.smartfleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFleetApplication.class, args);
+		System.out.println("Hello WOrld");
+	}
+
+}
---
+++ src/test/java/com/example/smartfleet/smartfleet/SmartFleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartfleet.smartfleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
