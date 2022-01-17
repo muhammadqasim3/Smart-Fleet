bedb9e2c6a09941b8fcd772dfd4963584006a9c1
Muhammad Qasim <muhammadqasimaslam3@gmail.com>
2022-01-17 10:03:38 +0500

    Sidebar & layout setup

--- src/main/java/com/example/smartfleet/smartfleet/DashboardController.java
+++ src/main/java/com/example/smartfleet/smartfleet/DashboardController.java
@@ -8,7 +8,37 @@ public class DashboardController {
 	
 	@GetMapping("index")
 	public String index() {
-		return "dashboard/_layout";
+		return "_layout";
+	}
+	
+	@GetMapping("hr")
+	public String hr() {
+		return "hr/index";
+	}
+	
+	@GetMapping("accounts")
+	public String accounts() {
+		return "accounts/index";
+	}
+	
+	@GetMapping("fleets")
+	public String fleets() {
+		return "fleets/index";
+	}
+	
+	@GetMapping("payroll")
+	public String payroll() {
+		return "payroll/index";
+	}
+	
+	@GetMapping("helpdesk")
+	public String helpdesk() {
+		return "helpdesk/index";
+	}
+	
+	@GetMapping("settings")
+	public String settings() {
+		return "settings/index";
 	}
 
 }
