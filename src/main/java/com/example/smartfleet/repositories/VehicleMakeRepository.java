b0b5d66188db9748745c167f52e1c3cd04bf2798
Muhammad Qasim <muhammadqasimaslam3@gmail.com>
2022-01-27 18:13:49 +0500

    repositories created

--- src/main/java/com/example/smartfleet/repositories/VehicleMakeRepository.java
+++ src/main/java/com/example/smartfleet/repositories/VehicleMakeRepository.java
@@ -2,9 +2,9 @@ package com.example.smartfleet.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.smartfleet.models.Country;
+import com.example.smartfleet.models.VehicleMake;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Country, Integer> {
+public interface VehicleMakeRepository extends JpaRepository<VehicleMake, Integer> {
 
 }
