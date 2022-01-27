c975ab53e67672847ff0825febbd67f5ed402809
Muhammad Qasim <muhammadqasimaslam3@gmail.com>
2022-01-27 13:09:34 +0500

    Controllers Added

---
+++ src/main/java/com/example/smartfleet/controllers/LocationController.java
@@ -0,0 +1,8 @@
+package com.example.smartfleet.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LocationController {
+
+}
--- src/main/java/com/example/smartfleet/models/Location.java
+++ src/main/java/com/example/smartfleet/models/Location.java
@@ -1,5 +1,42 @@
 package com.example.smartfleet.models;
 
-public class Location {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data // Helps lombok to generate all the getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id") // this line prevents infinite recursion, needed when using @OneToMany relationship
+public class Location {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String description;
+	private String details;
+	private String city;
+	private String address;
+	
+	@ManyToOne
+	@JoinColumn(name="countryid", insertable=false, updatable=false)
+	private Country country;
+	private Integer countryid;
+	
+	@ManyToOne
+	@JoinColumn(name="stateid", insertable=false, updatable=false)
+	private State state;
+	private Integer stateid;
+	
 }
